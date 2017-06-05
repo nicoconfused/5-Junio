9f4356f074d6850d2d0b6f976f0e38b10cff6697
nicoconfused <nicolas.rojas.v@ug.uchile.cl>
2017-06-05 14:49:00 -0400

    Add files via upload
    Prueba

---
+++ refs.php
@@ -0,0 +1,163 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Antecedentes</title>
+
+    <!-- Bootstrap Core CSS -->
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Theme CSS -->
+    <link href="css/clean-blog.min.css" rel="stylesheet">
+
+    <!-- Custom Fonts -->
+    <link href="vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <link href='https://fonts.googleapis.com/css?family=Lora:400,700,400italic,700italic' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800' rel='stylesheet' type='text/css'>
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+</head>
+
+<body>
+
+    <!-- Navigation -->
+    <nav class="navbar navbar-default navbar-custom navbar-fixed-top">
+        <div class="container-fluid">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header page-scroll">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                    <span class="sr-only">Toggle navigation</span>
+                    Menu <i class="fa fa-bars"></i>
+            </div>
+
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav navbar-right">
+                    <li>
+                        <a href="index.php">Artículos</a>
+                    </li>
+                    <li>
+                        <a href="bibliografia.php">Bibliografía</a>
+                    </li>
+                    <li>
+                        <a href="viz.php">Visualizaciones</a>
+                    </li>
+                    <li>
+                        <a href="refs.php">Antecedentes</a>
+                    </li>
+                </ul>
+            </div>
+            <!-- /.navbar-collapse -->
+        </div>
+        <!-- /.container -->
+    </nav>
+
+    <!-- Page Header -->
+    <!-- Set your background image for this header on the line below. -->
+    <header class="intro-header" style="background-image: url('img/img4.png')">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1">
+                    <div class="site-heading">
+                        <h1>Influencia de Animación</h1>
+                        <hr class="small">
+                        <span class="subheading">Visual/compositiva de producciones animadas anacrónicas en animaciones actuales.</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </header>
+
+    <!-- Main Content -->
+    <div class="container">
+      <p>Algunos de los antecendentes que corresponden a mi investigación son:</p>
+        <div class="row">
+          <div class="col-sm-8 col-md-6 col-lg-4">
+            <p align="justify">Segmentos de programación infantil emitidos en Chile durante la década de los 90's, hasta inicios de los 2000. Específicamente "El Club de los Tigritos" de CHV y "Zoolo TV" de Mega, que contaban con una parrilla programática con productos extranjeros producidos en EEUU y Japón.
+            </p>
+            <img src='http://24.media.tumblr.com/tumblr_lsvt8p2YF61qik1vho6_500.jpg' class="img-responsive">
+
+          </div>
+          <div class="col-sm-8 col-md-6 col-lg-4">
+            <p align="justify">Series producidas actualmente que cuenten con referencias estéticas, visuales y compositivas de producciones anacrónicas licenciadas en los países de origen de dichas series. Uno de los ejemplos más claros es Steven Universe de Rebecca Sugar, que cuenta con referencias de Sailor Moon, Utena, entre otros.
+            </p>
+            <img src='https://tocatoon.com/wp-content/uploads/2016/10/Steven-Universe-title-1280x768.png' class="img-responsive">
+          </div>
+          <div class="col-sm-8 col-md-6 col-lg-4">
+            <p align="justify">Estudios de animación en Chile. De los cuales se pueda constatar, ya sea a través de entrevistas u observación el uso de referentes anacrónicos en sus trabajos. Esto con el fin de establecer de qué forma, a través del uso de referencias, se pueden producir animaciones exitosas que incentiven la industria en Chile.
+            </p>
+            <img src='https://www.cntv.cl/cntv/site/artic/20160729/imag/foto_0000000420160729173858.jpg' class="img-responsive">
+          </div>
+            </div>
+        </div>
+    </div>
+
+  <hr>
+
+    <!-- Footer -->
+    <footer>
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1">
+                    <ul class="list-inline text-center">
+                        <li>
+                            <a href="#">
+                                <span class="fa-stack fa-lg">
+                                    <i class="fa fa-circle fa-stack-2x"></i>
+                                    <i class="fa fa-twitter fa-stack-1x fa-inverse"></i>
+                                </span>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                <span class="fa-stack fa-lg">
+                                    <i class="fa fa-circle fa-stack-2x"></i>
+                                    <i class="fa fa-facebook fa-stack-1x fa-inverse"></i>
+                                </span>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                <span class="fa-stack fa-lg">
+                                    <i class="fa fa-circle fa-stack-2x"></i>
+                                    <i class="fa fa-github fa-stack-1x fa-inverse"></i>
+                                </span>
+                            </a>
+                        </li>
+                    </ul>
+                    <p class="copyright text-muted">Copyright &copy; Nicolás Rojas V. 2017</p>
+                </div>
+            </div>
+        </div>
+      </div>
+    </footer>
+
+    <!-- jQuery -->
+    <script src="vendor/jquery/jquery.min.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+
+    <!-- Contact Form JavaScript -->
+    <script src="js/jqBootstrapValidation.js"></script>
+    <script src="js/contact_me.js"></script>
+
+    <!-- Theme JavaScript -->
+    <script src="js/clean-blog.min.js"></script>
+
+</body>
+
+</html>
